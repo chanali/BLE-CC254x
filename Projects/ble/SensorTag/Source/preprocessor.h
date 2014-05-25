e0a515c9b90f803506bce9299f010fce9c712079
chanali <alee.nb@gmail.com>
2014-05-25 13:20:27 +0800

    frist commit

---
+++ Projects/ble/SensorTag/Source/preprocessor.h
@@ -0,0 +1,13 @@
+
+// ali copy from build options of preprocessor
+#define INT_HEAP_LEN 3072
+#define HALNODEBUG
+#define OSAL_CBTIMER_NUM_TASKS 1
+#define HAL_AES_DMA TRUE
+#define HAL_DMA TRUE
+#define POWER_SAVING
+#define HAL_LCD FALSE
+#define HAL_LED TRUE
+#define HAL_I2C TRUE
+#define HAL_ADC FALSE
+
